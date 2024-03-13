47a5b7683c8bb37687bd76c8c366425661511603
tobibanks <tobiodogwu@gmail.com>
2024-03-14 00:45:52 +0100

    estore first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { faSearch, faUserCircle, faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+faSearch = faSearch;
+faUserCircle = faUserCircle;
+faHeart = faHeart;
+faShoppingCart = faShoppingCart;
+}
